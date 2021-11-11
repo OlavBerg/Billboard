c6b70e9a53b7182c65dd9a0d000c510d0663e2d5
OlavBerg <berg_olav@yahoo.se>
2021-11-11 20:45:51 +0100

    Made some h-files

---
+++ account.hpp
@@ -0,0 +1,29 @@
+#include <string>
+#include <stdbool.h>
+
+class Account {
+    private:
+    std::string company_name;
+    std::string password;
+    int paid_amount;
+    std::string message;
+
+    public:
+    std::string get_company_name(void);
+
+    //Returns true if the given password is identical to the account's password. Returns false otherwise.
+    bool password_correct(std::string& password); 
+
+    int get_paid_amount(void);
+
+    std::string get_message(void);
+
+    void change_company_name(std::string& new_name);
+
+    //If the given 'current_password' is correct, the account's password is changed to 'new_password'.
+    int change_password(std::string& new_password, std::string& current_password); 
+
+    void change_paid_amount(int new_amount);
+
+    void change_message(std::string& new_message);
+};
---
+++ billboard.hpp
@@ -0,0 +1,32 @@
+#include "linked_list.hpp"
+
+#include <stdbool.h>
+
+class Billboard {
+    private:
+    bool is_running; // Is true while the billboard is running. Is false otherwise.
+    Node* account_list;
+    Node* serial_port_list;
+    
+    // Sends the display information to the connected arduinos, i.e. what messages to display and the time for each message.
+    void send_display_info(void); 
+
+    // Main menu options
+    void log_in(void);
+    void create_account(void);
+    void turn_off(void); // Ends the program
+
+    // Account menu options
+    void change_company_name(void);
+    void changed_paid_amount(void);
+    void change_message(void);
+    void log_out(void);
+    void delete_account(void);
+
+    // Menus
+    void main_menu(void); // Runs the main menu
+    void account_menu(void); // Runs the account menu
+
+    public:
+    void run(void); //Runs the billboard
+};
